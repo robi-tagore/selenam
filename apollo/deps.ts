4a09350d1705efbb9508505444da538c61db6242
Probha Akanda <probhaakanda@gmail.com>
2024-08-22 00:16:36 +0600

    headers were modified

--- apollo/deps.ts
+++ apollo/deps.ts
@@ -5,14 +5,19 @@ function APOLLO_OUT(arg: any): void {
 }
 
 var ytdlOptions = {
-  // requestOptions: {
-    // headers: {
-      // "User-Agent":
-        // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-      // 'Cookie':
-      //   "SID=g.a000nAjfIxrgEJUr01ndptRb26vEkcuEniYgzK1Gq7Egp6oadmP_hLO28i-M8IY2OK7jqZyfjgACgYKAVoSARESFQHGX2MivvrJaGPmoetcShK1efInVhoVAUF8yKr2vh8RNJONjkfRm7VyojtB0076; APISID=_93TuflXZ9V-kMc-/Afq1eEJp_jZO5cnA2; SAPISID=9udwVk-n2xl96bYl/AlkZqNh_3dD4U4tlr; __Secure-1PAPISID=9udwVk-n2xl96bYl/AlkZqNh_3dD4U4tlr; __Secure-3PAPISID=9udwVk-n2xl96bYl/AlkZqNh_3dD4U4tlr; PREF=tz=Asia.Dhaka&f7=100; SIDCC=AKEyXzUjqNDtfouND9jJnZayw_tiARsC8CehMe2odPyRvBHRYx6au_isYt7Z7aWwWxMgI61QOw",
-    // },
-  // },
+  requestOptions: {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+      "Accept-Language": "en-US,en;q=0.9",
+      Accept: "application/json, text/plain, */*",
+      "Accept-Encoding": "gzip, deflate, br",
+      Connection: "keep-alive",
+      Referer: "https://www.youtube.com",
+      Cookie:
+        "VISITOR_INFO1_LIVE=Qp-Jvyrzpos; YSC=Y8N--RW12YE; LOGIN_INFO=AFmmF2swRgIhAI_HtcyqVnW8gOY8SwtIQ7UtIw4JcnkHO4hE2MOXwWSQAiEA8QpyPz3dnDQ8xNHdDAnBqBvm_tR_NqL2ZTz_j1p64m8:QUQ3MjNmeFdBNUhGbC10Tkg2LWtOanFRT0lBUVE3LXRuQWJPU255Z1ZaQVBjWkZmVGVfV19PZ21sa3NiTi1saGx4TjVxbnJ0Sm45dmFiT1IwRlp2OFVhcFNMelBWWWx4Z1JlQkNleF9hV2Y4MXFINkFwUnAtNFNMWEtZTE1FS29McUowV0pHem1GWFNhN1dtTXJUanJGcVpsVEFqVmlhc3ZB",
+    },
+  },
 };
 
 export { APOLLO_OUT, ytdlOptions };
